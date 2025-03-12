2f24c7d5dc4dc423bf38cdba79fdfb9334a9eed4
NaynZahir <Naynzahir@gmail.com>
2025-03-12 19:20:13 -0300

    componetizando

---
+++ src/components/CharacterCard.jsx
@@ -0,0 +1,10 @@
+function CharacterCard({ character }) {
+    return (
+      <div style={{ border: "1px solid #ccc", borderRadius: "10px", padding: "10px", margin: "10px", width: "200px", textAlign: "center" }}>
+        <img src={character.image} alt={character.name} style={{ width: "100px", borderRadius: "50%" }} />
+        <h3>{character.name}</h3>
+        <p>{character.status} - {character.species}</p>
+      </div>
+    );
+  }
+export default CharacterCard;
---
+++ src/components/SearchBar.jsx
@@ -0,0 +1,12 @@
+function SearchBar({ search, onSearch }) {
+    return (
+      <input 
+        type="text" 
+        placeholder="Search character..." 
+        value={search} 
+        onChange={onSearch} 
+        style={{ padding: "8px", width: "80%", maxWidth: "300px", marginBottom: "20px" }}
+      />
+    );
+  }
+export default SearchBar;
